72179314d252581974b72165bf67c2436180b44c
dsiles001 <156915596+dsiles001@users.noreply.github.com>
2024-02-15 08:04:50 -0500

    Add files via upload

---
+++ portscanner.py
@@ -0,0 +1,29 @@
+import socket
+
+openports = []
+
+def scan(target, port):
+    testsock = socket.socket()
+    try:
+        testsock.settimeout(2)
+        testsock.connect((domain, port))
+        print(f"{port} is open")
+    except:
+        print("Couldn't connect")
+        pass
+    finally:
+        testsock.close()
+
+
+s = socket.socket()
+
+domain = input("Enter Target Domain: ")
+IPaddr = socket.gethostbyname(domain)
+print(IPaddr)
+
+#for portnum in range(1000):
+scan(domain, 80)
+
+
+
+
